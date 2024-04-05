ce84f06d4ab1173c5b6e909cb7967c2ce6ee2b51
yahyojondev <nabijonovdeveloper@gmail.com>
2024-04-05 09:03:25 +0500

    hello world

--- src/Pages/admin/Admin.js
+++ src/Pages/admin/Admin.js
@@ -3,7 +3,23 @@ import React from 'react'
 function Admin() {
   return (
     <div className='admin'>
-        <h2>admin</h2>
+        <div className="container">
+            <div className="admin__wrapper">
+                <div className="admin__left">
+                    <h1 className="admin__title">Admin panel</h1>
+                    <p className="admin__left__text">
+ If you have a small business, SMS is one of the best ways to keep in contact with your customers. For things like order confirmations, booking reminders, special offers, and updates, texts work better than email—because people actually read them.
+
+Of course, you can't just grab your smartphone and start sending messages; you run a business. SMS marketing apps let you message customers directly, or automate the whole process, so they get sent a text based on your booking calendar, marketing list, or other integrated app. Best of all, your customers can even text you straight back.
+
+Don't miss a lead: automate your SMS messaging
+Learn how
+I've been covering technology for more than 10 years, and I've been responsible for updating this list for the past six years, with a little help from other Zapier app testers. In that time, I've deeply researched and tested several dozen of the top SMS apps for small businesses. Based on all those experiences, here are my picks for the best SMS marketing tools.
+                    </p>
+                </div>
+                    <img src="https://avatars.mds.yandex.net/i?id=636605e11feb273d6e1a3fbef50ba924315aab61-12322214-images-thumbs&n=13" alt="" className="admin__img" />
+            </div>
+        </div>
     </div>
   )
 }
